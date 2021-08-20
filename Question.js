7bb2bcc8e00121368d39969e9d3caa949a5475ce
SOOYEON YOO <dbtndus0527@gmail.com>
2021-08-20 14:55:32 +0900

    Update Question.js

--- Question.js
+++ Question.js
@@ -1,25 +1,34 @@
-
-
 //변수 선언
 var clickValue = 1;
 var selClickValue = 0;
+var mbtiClickValue = 0;
+var mbti = new String();
 
-//MBTI 지수 정리
-
+//MBTI 지수 
+var e = 0; 
+var s = 0;
+var f = 0;
+var p = 0;
+var i = 0;
+var n = 0;
+var t = 0;
+var j = 0;
 
 //1. 배경
 //배경 순차적으로 출력
-QueBackground = ['url("버블티_1.png")', 'url("버블티_2.png")', 'url("버블티_3.png")', 'url("버블티_4.png")', 'url("버블티_5.png")', 'url("버블티_6.png")', 'url("버블티_7.png")', 'url("버블티_8.png")', 'url("버블티_9.png")', 'url("버블티_10.png")', 'url("버블티_11.png")'];
+QueBackground = ['url("tea_bg/버블티_1.png")', 'url("tea_bg/버블티_2.png")', 'url("tea_bg/버블티_3.png")', 'url("tea_bg/버블티_4.png")',
+ 'url("tea_bg/버블티_5.png")', 'url("tea_bg/버블티_6.png")', 'url("tea_bg/버블티_7.png")', 'url("tea_bg/버블티_8.png")',
+  'url("tea_bg/버블티_9.png")', 'url("tea_bg/버블티_10.png")', 'url("tea_bg/버블티_11.png")'];
 
 //2. 질문창
 //질문 내용 리스트
 QuestionList = [
-    "버블티 가게에서 새로운 신상 버블티가 나왔다고 하네요! 새로 나온 신상 버블티라 맛이 궁금한데.. 당신은 어떻게 할 건가요?",
+    "버블티 가게에서 새로운 신상 버블티가 나왔다고 하네요! 새로 나온 버블티라 맛이 궁금한데.. 당신은 어떻게 할 건가요?",
     "바쁜 일상 속에 살아가던 당신, 드디어 주말에 시간이 나네요! 당신은 무엇을 할 건가요?",
     "당신은 오랜만에 모임을 갔습니다. 당신은 모임에서 사람들과 대화를 나눌 때....",
     "버블티 가게에서 버블티를 주문하고 나오기까지를 기다리는 중, 당신은 어떤 생각을 하고 있나요?",
     "친구가 내가 전에 갔었던 버블티 가게의 위치를 물어봅니다. 당신은 버블티 가게의 위치를 어떻게 가르쳐 줄 건가요?",
-    "당신이 직접 버블티를 만드려고 합니다. 버블티를 만들 때 당신은 어떻게 만들 건가요? <br> (당신은 버블티 가게에서 일하고 있기에 기본적인 버블티 만드는 방법과 필요한 재료를 알고 있습니다.)",
+    "당신이 직접 버블티를 만드려고 합니다. 버블티를 만들 때 당신은 어떻게 만들 건가요? <br>(당신은 버블티 가게에서 일하고 있기에 기본적인 버블티 만드는 방법과 필요한 재료를 알고 있습니다.)",
     "친한 친구가 버블티를 만들어서 맛이 어떠냐고 물어봅니다. 내가 버블티를 먹었을 때는 맛이 별로라고 느꼈는데... 그렇다면 당신은 어떻게 대답할 것인가요?",
     "집에 도착했는데 버블티가 식탁 위에 있습니다. 당신은 식탁에 있는 버블티를 보고 무슨 생각이 먼저 드나요?",
     "당신의 버블티 가게에 들어온 신입 직원이 계속 노력한 끝에 버블티 레시피를 다 외웠습니다. 이 직원에게 어떻게 칭찬해줄까요?",
@@ -31,7 +40,7 @@ QuestionList = [
 //3. 버튼
 //버튼 질문 내용 리스트
 btn1ContextList = [ //E, S, F, P 지수
-    "맛이 어떨지 궁금하니까 버블티 가게에서 신상 버블티를 먹고 어떤 맛인지 알고 싶어.",
+    "맛이 어떨지 궁금하니까 버블티 가게에서 새로 나온 버블티를 먹고 어떤 맛인지 알고 싶어.",
     "시간이 나는 주말에는 친구들이랑 만나서 놀아야지",
     '"아니 이 버블티가 정말 맛있다니까.." 주로 대화를 이끌어 나간다.',
     '“버블티가 언제 나오지...” 버블티가 언제 나올 지 생각한다.',
@@ -45,7 +54,7 @@ btn1ContextList = [ //E, S, F, P 지수
     '“음.. 황당하긴 한데 타피오카 펄이 도착하고 나서 다음에 만들지 뭐..” 하면서 준비했던 것들을 치운다.'
 ];
 btn2ContextList = [ //I, N, T, J 지수
-    "맛은 궁금하지만 일단 신상 버블티의 후기를 찾아볼래!",
+    "맛은 궁금하지만 일단 새로 나온 버블티의 후기를 찾아볼래!",
     "집에서 누워있으면서 쉬고 싶어",
     '“오오.. 그 버블티가 정말 맛있구나..” 주로 상대방의 이야기를 듣고 반응해준다.',
     '“내가 버블티 가게에서 일하게 된다면...” 버블티 가게의 주인이 된다는 가정으로 시작하여 상상의 나래를 펼친다.',
@@ -60,20 +69,60 @@ btn2ContextList = [ //I, N, T, J 지수
 ];
 
 //버튼 질문 출력하기
-
-//버튼 누를 때마다 점수 적용
-var clickValue;
-function init(){
+function init1(){
     var b1 = document.getElementById('b1');
     var b2 = document.getElementById('b2');
+    var sel1Context = document.getElementById("sel1Context");
+    var sel2Context = document.getElementById("sel2Context");
 
-    b1.addEventListener('click',clickBtn);
+    b1.addEventListener('click',mbtiElement);
     b1.addEventListener('click',bCilck);
-
-    b2.addEventListener('click',clickBtn);
+    b1.addEventListener('click',clickBtn);
+    
+    b2.addEventListener('click',mbtiElement);
     b2.addEventListener('click',bCilck);
+    b2.addEventListener('click',clickBtn);
 }
 
+//mbti 각 지수 증가
+function mbtiElement(){
+    if (mbtiClickValue<3){
+        if(event.target == b1 || event.target == sel1Context){
+            e+=1;
+        }
+        else if (event.target == b2 || event.target == sel2Context){
+            i+=1;
+        }
+    }
+    else if (mbtiClickValue < 6){
+        if(event.target == b1 || event.target == sel1Context){
+            s+=1;
+        }
+        else if (event.target == b2 || event.target == sel2Context){
+            n+=1;
+        }
+    }
+    else if(mbtiClickValue < 9){
+        if(event.target == b1 || event.target == sel1Context){
+            f+=1;
+        }
+        else if (event.target == b2 || event.target == sel2Context){
+            t+=1;
+        }
+    }
+    else if(mbtiClickValue < 12){
+        if(event.target == b1 || event.target == sel1Context){
+            p+=1;
+        }
+        else if (event.target == b2 || event.target == sel2Context){
+            j+=1;
+        }
+    }
+
+    mbtiClickValue+=1;
+}
+
+//질문 함수
 function clickBtn(){
     var queTitle = document.getElementById('qTitle');
     var bg = document.getElementById('bg');
@@ -87,23 +136,68 @@ function clickBtn(){
     }
 
     else if (clickValue==12){
-
+        window.location.href = "ResultPage.html?"+mbti;
     }
 
     //질문 문항 출력하기
     queContext.innerHTML = QuestionList[clickValue-1];
 }
 
-function bCilck(){
+function bCilck(){ 
     selClickValue+=1;
-    var sel1Context = document.getElementById("sel1Context");
-    var sel2Context = document.getElementById("sel2Context");
     
     if (selClickValue<12){
+        //선택 문항 출력 함수
         sel1Context.innerHTML = btn1ContextList[selClickValue];
         sel2Context.innerHTML = btn2ContextList[selClickValue];
     }
     else if (selClickValue==12){
+        //mbti 지수 구분하기
+        var num = 0;
 
+        function compareEI(a,b){
+            if (a>b){
+                mbti= "E";
+            }
+            else{
+                mbti = "I";
+            }
+            num++;
+        }
+
+        function compareSN(a,b){
+            if (a>b){
+                mbti = mbti.concat("S");
+            }
+            else{
+                mbti = mbti.concat("N");
+            }
+            num++;
+        }
+
+        function compareFT(a,b){
+            if (a>b){
+                mbti = mbti.concat("F");
+            }
+            else{
+                mbti = mbti.concat("T");
+            }
+            num++;
+        }
+
+        function comparePJ(a,b){
+            if (a>b){
+                mbti = mbti.concat("P");
+            }
+            else{
+                mbti = mbti.concat("J");
+            }
+            num++;
+        }
+
+        compareEI(e,i);
+        compareSN(s,n);
+        compareFT(f,t);
+        comparePJ(p,i);
     }
 } 
